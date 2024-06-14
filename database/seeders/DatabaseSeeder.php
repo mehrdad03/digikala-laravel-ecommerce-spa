e39d5428079a5e4c8e76c09784096afa4ebb88d0
LeGion <mehrdad.dadashi@gmail.com>
2024-06-14 14:47:50 +0300

    Create StateSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        $this->call(CountrySeeder::class);
+        $this->call([
+            CountrySeeder::class,
+            StateSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/StateSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\State;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $states = [
+            'آذربایجان شرقی', 'آذربایجان غربی', 'اردبیل', 'اصفهان', 'البرز', 'ایلام',
+            'بوشهر', 'تهران', 'چهارمحال و بختیاری', 'خراسان جنوبی', 'خراسان رضوی', 'خراسان شمالی',
+            'خوزستان', 'زنجان', 'سمنان', 'سیستان و بلوچستان', 'فارس', 'قزوین', 'قم', 'کردستان',
+            'کرمان', 'کرمانشاه', 'کهگیلویه و بویراحمد', 'گلستان', 'گیلان', 'لرستان', 'مازندران',
+            'مرکزی', 'هرمزگان', 'همدان', 'یزد'
+        ];
+
+
+        foreach ($states as $state) {
+            State::query()->create([
+                'name' => $state,
+                'country_id' => 22,
+            ]);
+        }
+    }
+}
