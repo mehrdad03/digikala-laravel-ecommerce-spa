6cd67343b1e55063e43ce8dc118d7a8d9340dd86
LeGion <mehrdad.dadashi@gmail.com>
2024-06-20 16:18:31 +0300

    Cascade delete product is done

--- app/Livewire/Admin/Product/Index.php
+++ app/Livewire/Admin/Product/Index.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin\Product;
 
 use App\Models\Product;
+use App\Models\SeoItem;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,11 +11,27 @@ class Index extends Component
 {
     use WithPagination;
 
+
+    public function delete($productId)
+    {
+
+        $product=Product::query()->where('id',$productId)->first();
+        if ($product){
+            $product->delete();
+        }
+        //delete product from products table
+
+        //delete seo items from seoItems table
+        SeoItem::query()->where('ref_id',$productId)->delete();
+        //delete image from productImages table
+
+    }
+
     public function render()
     {
 
         $products = Product::query()
-            ->with('category','coverImage')
+            ->with('category', 'coverImage')
             ->latest()
             ->paginate(10);
         return view('livewire.admin.product.index', [
--- database/migrations/2024_06_10_172931_create_product_images_table.php
+++ database/migrations/2024_06_10_172931_create_product_images_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('path');
             $table->boolean('is_cover')->default(false);
-            $table->foreignId('product_id')->constrained();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
