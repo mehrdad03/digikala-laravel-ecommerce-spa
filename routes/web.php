9ecf02f957dbb0bba6fe46139476c50edbb55998
LeGion <mehrdad.dadashi@gmail.com>
2024-07-16 11:44:04 +0300

    refactor client route

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function (){
+Route::prefix('admin')->group(function () {
     require __DIR__ . '/admin.php';
 });
 
-Route::prefix('client')->group(function () {
-    require __DIR__ . '/client.php';
-});
+
+require __DIR__ . '/client.php';
+
 
 Route::prefix('seller')->group(function () {
     require __DIR__ . '/seller.php';
