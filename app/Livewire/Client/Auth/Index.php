023ec54773c7f2a5d02c17bcc2647013ac9fdfa1
LeGion <mehrdad.dadashi@gmail.com>
2024-08-08 18:23:49 +0300

    Create client home page component

--- app/Livewire/Client/Auth/Index.php
+++ app/Livewire/Client/Auth/Index.php
@@ -109,6 +109,6 @@ class Index extends Component
 
     public function render()
     {
-        return view('livewire.client.auth.index')->layout('layouts.client.app');
+        return view('livewire.client.auth.index')->layout('layouts.client.app-auth');
     }
 }
--- routes/client.php
+++ routes/client.php
@@ -5,9 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 
 use App\Livewire\Client\Auth\Index as authIndex;
+use App\Livewire\Client\Home\Index as homeIndex;
 
 Route::name('client.')->group(function (){
-    Route::get('/', authIndex::class)->name('home');
+    Route::get('/', homeIndex::class)->name('home');
     Route::get('/auth', authIndex::class)->name('auth.index');
     Route::get('/gmail', [authIndex::class,'redirectToProvider'])->name('gmail');
     Route::get('/auth/gmail/callback', [authIndex::class,'handleProviderCallback'])->name('gmail.callback');
