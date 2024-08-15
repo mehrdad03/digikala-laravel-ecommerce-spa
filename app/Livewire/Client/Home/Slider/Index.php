2a3127653a8946d11b15d5122c9c8121e0556b35
LeGion <mehrdad.dadashi@gmail.com>
2024-08-15 19:20:55 +0300

    feat: Add dynamic sliders section to user interface
    - Added new sliders section that fetches and displays data from the database
    - Implemented dynamic rendering to handle various sliders formats
    - Updated view to include stories component

--- app/Livewire/Client/Home/Slider/Index.php
+++ app/Livewire/Client/Home/Slider/Index.php
@@ -2,15 +2,24 @@
 
 namespace App\Livewire\Client\Home\Slider;
 
+use App\Models\Slider;
 use Livewire\Component;
 
 class Index extends Component
 {
 
+    public $sliders = [];
+
+    public function mount()
+    {
+        $this->sliders = Slider::query()->where('status', '=', true)->get();
+    }
+
     public function placeholder()
     {
         return view('Layouts.client.placeholders.first-page.slider-skeleton');
     }
+
     public function render()
     {
         return view('livewire.client.home.slider.index');
--- resources/views/livewire/client/home/slider/index.blade.php
+++ resources/views/livewire/client/home/slider/index.blade.php
@@ -39,24 +39,13 @@
                     aria-label="Slide 6"></button>
             </div>
             <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="2000">
-                    <img src="/client/assets/slider/1.webp" class="d-block w-100" alt="..."/>
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="/client/assets/slider/2.webp" class="d-block w-100" alt="..."/>
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="/client/assets/slider/3.webp" class="d-block w-100" alt="..."/>
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="/client/assets/slider/4.webp" class="d-block w-100" alt="..."/>
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="/client/assets/slider/5.webp" class="d-block w-100" alt="..."/>
-                </div>
-                <div class="carousel-item" data-bs-interval="3500">
-                    <img src="/client/assets/slider/6.gif" class="d-block w-100" alt="..."/>
-                </div>
+
+                @foreach($sliders as $item)
+                    <div class="carousel-item {{$loop->index==0 ?'active':''}}" data-bs-interval="2000">
+                        <img src="sliders/{{$item->image}}" class="d-block w-100" alt="{{$item->title}}" title="{{$item->title}}"/>
+                    </div>
+                @endforeach
+
             </div>
             <button
                 class="carousel-control-prev"
